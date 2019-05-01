c9e68a86df0049d148dcb80bd3fdbaee96629695
Nicolás Hernández <nm.hernandez10@uniandes.edu.co>
2019-05-01 13:37:02 -0500

    topics.json pero sin funcionar

--- reports_app/lib/views/topics.dart
+++ reports_app/lib/views/topics.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:vertical_tabs/vertical_tabs.dart';
+import 'dart:io';
+import 'dart:convert';
 
 class TopicsView extends StatelessWidget {
+
+  final contentRoute = "assets/content/topics.json";
+  final Map data;
+
+  TopicsView([this.data]);
+
   @override
   Widget build(BuildContext context) {
+
+    new File(contentRoute)
+    .readAsString()
+    .then((fileContents) => jsonDecode(fileContents))
+    .then((jsonData) {
+        print(jsonData);
+    });
+
     double deviceWidth = MediaQuery.of(context).size.width;
     double sizeForIcons = deviceWidth * 0.07;
     Color colorForIcons = Colors.grey.shade700;
@@ -32,31 +48,24 @@ class TopicsView extends StatelessWidget {
                        child: Center(child: Icon(Icons.settings, size: sizeForIcons, color: colorForIcons,)),
                     ),
                     Tab(
-                      child: Center(child: Icon(Icons.error_outline, size: sizeForIcons, color: colorForIcons,)),
+                      child: Center(child: Icon(Icons.edit, size: sizeForIcons, color: colorForIcons,)),
                     ),
                     Tab(
-                       child: Center(child: Icon(Icons.phone, size: sizeForIcons, color: colorForIcons,)),
-                    ),
-                    Tab(
-                       child: Center(child: Icon(Icons.memory, size: sizeForIcons, color: colorForIcons,)),
+                       child: Center(child: Icon(Icons.battery_charging_full, size: sizeForIcons, color: colorForIcons,)),
                     ),
                     Tab(
                        child: Center(child: Icon(Icons.signal_cellular_4_bar, size: sizeForIcons, color: colorForIcons,)),
                     ),
                     Tab(
-                       child: Center(child: Icon(Icons.multiline_chart, size: sizeForIcons, color: colorForIcons,)),
-                    ),
-                    Tab(
-                       child: Center(child: Icon(Icons.edit, size: sizeForIcons, color: colorForIcons,)),
+                       child: Center(child: Icon(Icons.storage, size: sizeForIcons, color: colorForIcons,)),
                     )
                   ],
                   contents: <Widget>[
-                    tabsContent('Flutter',
-                        'Change page by scrolling content is disabled in settings. Changing contents pages is only available via tapping on tabs'),
+                    tabsContent('Flutter'),
                     tabsContent('Dart'),
                     tabsContent('Javascript'),
                     tabsContent('NodeJS'),
-                    Container(
+                    /* Container(
                         color: Colors.black12,
                         child: ListView.builder(
                             itemCount: 10,
@@ -66,9 +75,9 @@ class TopicsView extends StatelessWidget {
                                 margin: EdgeInsets.all(10),
                                 color: Colors.white30,
                               );
-                            })),
+                            })), */
                     tabsContent('HTML 5'),
-                    Container(
+                   /*  Container(
                         color: Colors.black12,
                         child: ListView.builder(
                             scrollDirection: Axis.horizontal,
@@ -79,7 +88,7 @@ class TopicsView extends StatelessWidget {
                                 margin: EdgeInsets.all(10),
                                 color: Colors.white30,
                               );
-                            })),
+                            })), */
                   ],
                 ),
               ),
@@ -90,7 +99,7 @@ class TopicsView extends StatelessWidget {
     );
   }
 
-  Widget tabsContent(String caption, [String description = '']) {
+  Widget tabsContent(String key) {
     return Container(
       margin: EdgeInsets.all(10),
       padding: EdgeInsets.all(20),
@@ -98,7 +107,7 @@ class TopicsView extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Text(
-            caption,
+            "xd",
             style: TextStyle(fontSize: 25),
           ),
           Divider(
@@ -106,7 +115,7 @@ class TopicsView extends StatelessWidget {
             color: Colors.black45,
           ),
           Text(
-            description,
+            "xd",
             style: TextStyle(fontSize: 15, color: Colors.black87),
           ),
         ],
